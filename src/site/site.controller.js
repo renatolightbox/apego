b55bd44bdeb4a1d42a1e8834b5ab6a0cdf4adf07
KarinaPiestun <kpiestun@icloud.com>
2021-06-08 17:14:48 -0300

    site.controller site.routes

--- src/site/site.controller.js
+++ src/site/site.controller.js
@@ -1,10 +1,64 @@
 class SiteController {
+
+    static BoletosPage(req, res) {
+        res.render('boletos', { title: 'Express' });
+    }
+
+    static ChatPage(req, res) {
+        res.render('chat', { title: 'Express' });
+
+    }
+
+    static ContatoPage(req, res) {
+        res.render('contato', { title: 'Express' });
+
+    }
+
+    static CriarLoginPage(req, res) {
+        res.render('criarLogin', { title: 'Express' });
+
+    }
+
+    static IndexPage(req, res) {
+        res.render('index', { title: 'Express' });
+    }
+
+    static LoginPage(req, res) {
+        res.render('login', { title: 'Express' });
+    }
+
     static MenuPage(req, res) {
         res.render('menu', { title: 'Express' });
     }
 
-    static IndexPage(req,res) {
-        res.render('index', { title: 'Express' });
+
+    static MuralPage(req, res) {
+        res.render('mural', { title: 'Express' });
+
     }
+
+    static PerfilPage(req, res) {
+        res.render('perfil', { title: 'Express' });
+
+    }
+
+    static QuemSomosPage(req, res) {
+        res.render('quem somos', { title: 'Express' });
+
+    }
+
+    static ReservasPage(req, res) {
+        res.render('reservas', { title: 'Express' });
+    }
+
+
+
+
+
+
 }
+
 module.exports = SiteController
+
+
+
--- src/site/site.routes.js
+++ src/site/site.routes.js
@@ -4,6 +4,14 @@ const router = express.Router()
 
 router.get('/menu', SiteController.MenuPage)
 router.get('/', SiteController.IndexPage)
-
+router.get('/boletos' , SiteController.BoletosPage)
+router.get('/chat' ,SiteController.ChatPage )
+router.get('/contato' ,SiteController.ContatoPage)
+router.get('/criarLogin' ,SiteController.CriarLoginPage)
+router.get('/login' , SiteController.LoginPage)
+router.get('/mural' ,SiteController.MuralPage)
+router.get('/perfil' , SiteController.PerfilPage)
+router.get('/QuemSomos' , SiteController.QuemSomosPage)
+router.get('/reservas' , SiteController.ReservasPage )
 
 module.exports = router;
